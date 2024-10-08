9f6daacd178dd7d9b7721ad39778190317d16bc6
Humblot Nour-Teylor <nourteylor@gmail.com>
2024-10-08 18:48:35 +0200

    Commit debug cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,4 +7,16 @@ def test_top_countries():
     string = StringIO()
     cli.top_countries(file=string)
     text = string.getvalue()
-    assert 'Top' in text
+    assert 'Top 10 countries' in text
+
+def test_top_collective():
+    string = StringIO()
+    cli.top_collective(file=string)
+    text = string.getvalue()
+    assert 'Top 10 collective events' in text
+
+def test_top_individual():
+    string = StringIO()
+    cli.top_individual(file=string)
+    text = string.getvalue()
+    assert 'Top 10 individual events' in text
